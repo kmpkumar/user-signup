d87849a9f40e348c47bdebec720448c1b2d9f6ff
Maruthi Korrapati <kmpkumar@gmail.com>
2018-09-26 22:29:27 -0500

    initial file creation

---
+++ main.py
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <body>
+        <from ="/" method='POST'>
+        <label>Username
+        <input name="" type='text' value=''/>
+        </label>
+        <br/>
+        <label>Username
+        <input name="" type='text' value=''/>
+        </label>
+        <label>Password
+        <br/>
+        <input name="" type='text' value=''/>
+        </label>
+        <label>Verify Password
+        <br/>
+        <input name="" type='text' value=''/>
+        </label>
+        <label>Email(optional)
+        <br/>
+        <input name="" type='text' value=''/>
+        </label>
+        <br/>
+        <input type="Submit" value='Submit'/>
+        </label>
+        </body>
+    </head>
+
+</html>
